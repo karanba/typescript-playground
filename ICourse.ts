9725e7c3aa843b358ec202d8a29ed4608c6bc2ef
karanba <altaykarakus@gmail.com>
2023-10-01 12:42:21 +0300

    1 file modified, 1 added

---
+++ ICourse.ts
@@ -0,0 +1,5 @@
+export interface ICourse {
+  title: string;
+  subTitle: string;
+  lessonsCount: number;
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,35 @@
 // Import stylesheets
 import './style.css';
+import { ICourse } from './ICourse.ts';
 
-// Write TypeScript code!
+let course: ICourse = {
+  title: 'TypeScript BootCamp',
+  subTitle: 'Learn Fundamentals of TypeScript',
+  lessonsCount: 10,
+};
+
+console.log(course);
+console.log('course type: ', typeof course);
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+type Employee = {
+  position: string;
+  salary: number;
+};
+
+type PersonEmployee = Person & Employee;
+
+const john: PersonEmployee = {
+  name: 'John',
+  age: 30,
+  position: 'Manager',
+  salary: 50000,
+};
+
+console.log(john);
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
